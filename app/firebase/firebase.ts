837e55da0c446646b9885acdcfa4eff856b79b64
mmatijevic <mmatijevic@collectivemind.dev>
2023-10-06 08:35:33 +0200

    Added firebase and other configs

---
+++ app/firebase/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB63uqdHbZFL6k5rkr9HYqkNiiNpdq7N8g',
+  authDomain: 'devot-challenge.firebaseapp.com',
+  projectId: 'devot-challenge',
+  storageBucket: 'devot-challenge.appspot.com',
+  messagingSenderId: '1068726912796',
+  appId: '1:1068726912796:web:e2eebb4006ca5fea36dcd9',
+  measurementId: 'G-30QXF4W06Q'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
