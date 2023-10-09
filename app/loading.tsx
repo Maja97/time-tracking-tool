e2af8954ac7cd425e5b1ec6cb27bb051dec9a92e
mmatijevic <mmatijevic@collectivemind.dev>
2023-10-09 13:40:38 +0200

    Edited 404 page and added loading

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '@app/styles/loading.module.scss';
+
+function Loading() {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.overlayInner}>
+        <div className={styles.overlayContent}>
+          <span className={styles.spinner}></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,16 +6,14 @@ import { Routes } from './consts/routes';
 
 export default function NotFound() {
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.text}>
-          <h1 className={styles.title}>{strings.notFound.title}</h1>
-          <p className={styles.message}>{strings.notFound.message}</p>
-        </div>
-        <Button className={styles.button} variant="secondary">
-          <Link href={Routes.Home}>{strings.notFound.link}</Link>
-        </Button>
+    <div className={styles.wrapper}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{strings.notFound.title}</h1>
+        <p className={styles.message}>{strings.notFound.message}</p>
       </div>
-    </>
+      <Button className={styles.button} variant="secondary">
+        <Link href={Routes.Home}>{strings.notFound.link}</Link>
+      </Button>
+    </div>
   );
 }
