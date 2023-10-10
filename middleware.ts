143c0628eece3fc89707f5b560ea7f74272f08f9
mmatijevic <mmatijevic@collectivemind.dev>
2023-10-10 14:37:00 +0200

    Reordering folders

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import cookieKeys from '@app/consts/cookies';
-import { Routes } from '@app/consts/routes';
+import cookieKeys from '@app/_consts/cookies';
+import { Routes } from '@app/_consts/routes';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
